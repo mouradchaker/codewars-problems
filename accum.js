039a69cc0251b42e9972231d8f5ad36bbc132748
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-05 23:45:56 +0100

    accum

---
+++ accum.js
@@ -0,0 +1,12 @@
+function accum(s) {
+  var letters = s.split(""),
+    words = [];
+
+  for (var i = 0; i < letters.length; i++) {
+    words.push(
+      letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase())
+    );
+  }
+
+  return words.join("-");
+}
